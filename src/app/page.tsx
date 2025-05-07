c2104eb9e290dc478965d2465659fedf6c8db1b7
dzx <dzxdetianxuan@outlook.com>
2025-05-07 15:58:04 +0800

    1.1完成了prisma的初始化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button>haha</Button>
+    <div className="w-screen h-screen flex flex-col gap-20 bg-primary">
+      <Button>haha</Button>
+      <div className="w-20 h-39 bg-primary border border-border text-accent-foreground">bsd</div>
+      <input className="w-200 border border-input"></input>
+    </div>
   );
 }
