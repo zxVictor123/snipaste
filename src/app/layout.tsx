156b016cff52851f2ee8cb39c8463e926982179d
dzx <dzxdetianxuan@outlook.com>
2025-05-08 17:31:39 +0800

    1.3完成了新增页面的提交到数据库

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="container mx-auto px-12">
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,36 +1,54 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { db } from '@/db'
+import { redirect } from 'next/navigation'
 
 import React from 'react'
 
 function page() {
+  async function createSnippet(formData: FormData) {
+    'use server'
+
+    const title = formData.get('title') as string
+    const code = formData.get('code') as string
+
+    const snippet =  await db.snippet.create({
+      data: {
+          title: title,
+          code: code,
+      }
+  })
+    console.log(snippet)
+    redirect('/')
+  
+  }
   return (
-    <div className="flex w-screen h-screen justify-center items-center bg-background">
+    <div className="flex-center h-screen bg-background">
 
       {/* 卡片 */}
-      <div className="bg-card w-150 flex flex-col justify-center gap-md border border-border rounded-xl p-3 text-foreground">
+      <form action={createSnippet} className="bg-card w-full flex flex-col justify-center gap-md border border-border rounded-xl p-3 text-foreground">
 
         {/* 标题 */}
-        <p className="text-accent-foreground font-black text-xl">
+        <h2 className="text-accent-foreground font-black text-xl mx-auto">
           Create a Snippet
-        </p>
+        </h2>
 
         {/* 输入框区域 */}
         <div className="flex flex-col gap-sm">
           <div className="flex justify-between gap-sm">
-            <p>Title:</p>
-            <Input className="w-9/10"></Input>
+            <label>Title:</label>
+            <Input name= 'title' className="w-9/10"></Input>
           </div>
           <div className="flex justify-between gap-sm">
-            <p>Code:</p>
-            <Input className="w-9/10"></Input>
+            <label>Code:</label>
+            <Input name='code' className="w-9/10"></Input>
           </div>
         </div>
 
         {/* 创建按钮 */}
-        <Button className="bg-primary">Create</Button>
+        <Button type='submit' className="bg-primary">Create</Button>
 
-      </div>
+      </form>
 
     </div>
   )
